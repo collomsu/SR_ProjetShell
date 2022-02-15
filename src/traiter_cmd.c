1086da51fdcb366c97af610104b59d5ded83447c
rimbault <titouan.rimbault@hotmail.fr>
2022-02-15 15:57:30 +0100

    Avancée 3.2

--- src/traiter_cmd.c
+++ src/traiter_cmd.c
@@ -3,14 +3,60 @@
 retoursTraitementCommande traiter_commande(struct cmdline *l) {
   retoursTraitementCommande retour = NORMAL;
 
-  //Commandes de sortie du Shell
-  if(strcmp(l->seq[0][0], "exit") == 0 || strcmp(l->seq[0][0], "quit") == 0)
+  int i;
+
+  //Si la commande est une commande simple
+  if(l->seq[1] == NULL)
   {
-    retour = FERMETURE_SHELL;
+    //On commence par contrôler si la commande n'est pas une commande interne au Shell
+    retour = traiter_commande_interne(l->seq[0]);
+    
+    //Si la commande n'est pas une commande interne
+    if(retour == COMMANDE_INTERNE_PAS_TROUVEE)
+    {
+      //On créé un processus fils qui va exécuter la commande en question
+      int pidFils = fork();
+
+      if(pidFils == 0)
+      {
+        retoursTraitementCommande retourLancementCommande = execvp(l->seq[0][0], l->seq[0]);
+
+        if(retourLancementCommande == -1)
+        {
+          i = 0;
+
+          printf("Erreur lors du lancement de la commande \"");
+          while (l->seq[0][i] != NULL)
+          {
+            printf("%s", l->seq[0][i]);
+
+            i = i + 1;
+          }
+          printf("\".\n");
+
+          perror("exec");
+
+          exit(0);
+        }
+      }
+    }
   }
   else
   {
-    
+
+  }
+
+  return retour;
+}
+
+retoursTraitementCommande traiter_commande_interne(char **commande)
+{
+  retoursTraitementCommande retour = COMMANDE_INTERNE_PAS_TROUVEE;
+
+  //Commandes de sortie du Shell
+  if(strcmp(commande[0], "exit") == 0 || strcmp(commande[0], "quit") == 0)
+  {
+    retour = FERMETURE_SHELL;
   }
 
   return retour;
--- src/traiter_cmd.h
+++ src/traiter_cmd.h
@@ -5,15 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include "errno.h"
 #include "readcmd.h"
+#include "csapp.h"
 
 //Enum utilisée pour gérer le retour du traitement de commandes
 typedef enum {
     NORMAL,
+    COMMANDE_INTERNE_PAS_TROUVEE,
     //Valeur indiquant que le Shell doit être fermé
     FERMETURE_SHELL
 } retoursTraitementCommande;
 
 retoursTraitementCommande traiter_commande(struct cmdline *l);
+retoursTraitementCommande traiter_commande_interne(char **commande);
 
 #endif
