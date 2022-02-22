5ea8833e4a5a30c52c3758f79d305f95be2bbd70
collomsu <39761559+collomsu@users.noreply.github.com>
2022-02-22 17:24:13 +0100

    Partie 3.6 : Update du fichier testpipes.c
    Modification du fichier en incluant la librairie csapp et en retirant des librairies inutiles.

--- src/testpipes.c
+++ src/testpipes.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include "csapp.h"
 
 void pipeline( char * ar[], int pos, int in_fd);
 void error_exit(const char*);
@@ -12,19 +11,18 @@ int main(int argc, char * argv[]) {
     printf("Usage: %s option (option) ...\n", argv[0]);
     exit(1);
   }
-  pipeline(argv, 1, STDIN_FILENO);
+  pipeline(argv, 1, 0);
   return 0;
 }
 
 void error_exit(const char *kom) {
   perror(kom);
-  (child ? _exit : exit)(EXIT_FAILURE);
 }
 
 void pipeline(char *ar[], int pos, int in_fd){
   if(ar[pos+1] == NULL) { /*last command */
-    if(in_fd != STDIN_FILENO) {
-      if(dup2(in_fd, STDIN_FILENO) != -1)
+    if(in_fd != 0) {
+      if(dup2(in_fd, 0) != -1)
         close(in_fd); /*successfully redirected*/
       else error_exit("dup2");
     }
@@ -40,9 +38,9 @@ void pipeline(char *ar[], int pos, int in_fd){
     if (childpid == 0) { /* child executes current command */
       child = 1;
       close(fd[0]);
-      if (dup2(in_fd, STDIN_FILENO) == -1) /*read from in_fd */
+      if (dup2(in_fd, 0) == -1) /*read from in_fd */
         perror("Failed to redirect stdin");
-      if (dup2(fd[1], STDOUT_FILENO) == -1)   /*write to fd[1]*/
+      if (dup2(fd[1], 1) == -1)   /*write to fd[1]*/
         perror("Failed to redirect stdout");
       else if ((close(fd[1]) == -1))
         perror("Failed to close extra pipe descriptors");
